53c9b90b1346fabe708d1f6f4cf73a408a100198
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-08-28 09:12:25 -0500

    updated index.tsx

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,10 +3,10 @@ import Head from "next/head";
 import { Tabs, TabList, Tab, Checkbox, Button, TabPanel, TabPanels, TextInput, Accordion, AccordionItem } from '@carbon/react';
 import { string } from "zod";
 
-export interface SectionProps {
-  title: string;
-  image: string;
-  info: string;
+export interface ListProps {
+  name: string;
+  email: string;
+  phone: string;
 }
 
 const Home: NextPage = () => {
@@ -19,86 +19,74 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-      <Tabs>
-  <TabList
-    aria-label="List of tabs"
-    contained
-  >
-    <Tab>
-      Tesla
-    </Tab>
-    <Tab>
-      Lucid
-    </Tab>
-    <Tab>
-      Rivian
-    </Tab>
-    <Tab>
-      Fisker
-    </Tab>
-    <Tab>
-      NIO
-    </Tab>
-  </TabList>
-  <TabPanels>
-    <TabPanel>
-      <CarSection 
-        title="One"
-        image="/modelS2014.jpg"
-        info="Testing the props"
-      />
-    </TabPanel>
-    <TabPanel>
-      <form
-        style={{
-          margin: '2em'
-        }}
-      >
-        <legend className="cds--label">
-          Validation example
-        </legend>
-        <Checkbox
-          id="cb"
-          labelText="Accept privacy policy"
-        />
-        <Button
-          style={{
-            marginBottom: '1rem',
-            marginTop: '1rem'
-          }}
-          type="submit"
-        >
-          Submit
-        </Button>
-        <TextInput
-          helperText="Optional help text"
-          labelText="Text input label"
-          type="text"
-        />
-      </form>
-    </TabPanel>
-    <TabPanel>
-      Tab Panel 3
-    </TabPanel>
-    <TabPanel>
-      Tab Panel 4
-    </TabPanel>
-    <TabPanel>
-      Tab Panel 5
-    </TabPanel>
-  </TabPanels>
-</Tabs>
+        <Tabs>
+          <TabList
+            aria-label="List of tabs"
+            contained
+          >
+            <Tab>
+              Contacts
+            </Tab>
+            <Tab>
+              Add Contact
+            </Tab>
+            <Tab>
+              Account
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <ContactList 
+                name="John Doe"
+                email="test@email.com"
+                phone="+1 (123)456-7890"
+              />
+            </TabPanel>
+            <TabPanel>
+              <form
+                style={{
+                  margin: '2em'
+                }}
+              >
+                <legend className="cds--label">
+                  Validation example
+                </legend>
+                <Checkbox
+                  id="cb"
+                  labelText="Accept privacy policy"
+                />
+                <Button
+                  style={{
+                    marginBottom: '1rem',
+                    marginTop: '1rem'
+                  }}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+                <TextInput
+                  helperText="Optional help text"
+                  labelText="Text input label"
+                  type="text"
+                />
+              </form>
+            </TabPanel>
+            <TabPanel>
+              Tab Panel 3
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </main>
     </>
   );
 };
 
-const CarSection = ({title, image, info}: SectionProps) => {
+const ContactList = ({name, email, phone}: ListProps) => {
   return (
     <Accordion align="start">
-      <AccordionItem title={title}>
-        <img src={image}/>
-        {info}
+      <AccordionItem title={name}>
+        <p>{email}</p>
+        <p>{phone}</p>
       </AccordionItem>
     </Accordion>
   )
